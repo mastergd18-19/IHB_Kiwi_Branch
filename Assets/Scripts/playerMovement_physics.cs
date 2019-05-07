3509a4afe518d083efc648c3d16e78acdff828c0
Iñaki <inaki.haba@live.u-tad.com>
2019-05-07 18:58:47 +0200

    Added new script - Movement by physics

---
+++ Assets/Scripts/playerMovement_physics.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement_physics : MonoBehaviour
+{
+
+
+    private Rigidbody rb;
+    public float acceleration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        Movement();
+
+    }
+
+
+    void Movement()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * acceleration);
+
+
+    }
+
+}
