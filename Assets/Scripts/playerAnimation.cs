495f2156d5889b01feaa26ad54bcb8754c7256df
inakikiwi <inakikiwi@users.noreply.github.com>
2019-05-21 21:36:16 +0200

    Added animations

---
+++ Assets/Scripts/playerAnimation.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAnimation : MonoBehaviour
+{
+
+    playerMovement_Traslation pm;
+    Animator Anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       pm = this.gameObject.GetComponent<playerMovement_Traslation>();
+       Anim = this.gameObject.GetComponentInParent<Animator>();
+       Anim.SetBool("Center", true);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (pm.rightMovement)
+        {
+            Debug.Log("TEST center = " + pm.center);
+
+            if (pm.center)
+            {
+                Debug.Log("TEST COUNT = " + pm.right);
+                Anim.SetBool("Right", pm.right);
+                Anim.SetBool("Center", pm.center);
+            }
+            else if (pm.left)
+            {
+                Anim.SetBool("Right", pm.right);
+                Anim.SetBool("Center", pm.center);
+            }
+            
+        }
+
+        if (pm.leftMovement)
+        {
+            if (pm.center)
+            {
+                Anim.SetBool("Center", pm.center);
+                Anim.SetBool("Left", pm.left);
+            }
+            else if(pm.right)
+            {
+                Anim.SetBool("Center", pm.center);
+                Anim.SetBool("Left", pm.left);
+            }
+
+        }
+
+        if (!pm.moving)
+        {
+            Anim.SetBool("Idle", true);
+        }
+        else
+        {
+            Anim.SetBool("Idle", false);
+        }
+
+    }
+}
--- Assets/Scripts/playerMovement_Traslation.cs
+++ Assets/Scripts/playerMovement_Traslation.cs
@@ -9,17 +9,24 @@ public class playerMovement_Traslation : MonoBehaviour
     public float basicSpeed  = 10;
     public bool moving;
 
-    public bool right;
-    public bool left;
-    public bool center;
+    public bool right = false;
+    public bool left = false;
+    public bool center = false;
+    public bool rightMovement = false;
+    public bool leftMovement = false;
+    public float count = 10;
 
-    public Vector3 distancia = new Vector3(3,0,0);
+    Animator Anim;
+
+    public Vector3 distancia = new Vector3(30,0,0);
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        center = true;
+        Anim = GetComponent<Animator>();
+       
     }
 
     // Update is called once per frame
@@ -30,28 +37,77 @@ public class playerMovement_Traslation : MonoBehaviour
 
     }
 
-    void Movement()
+    public void Movement()
     {
 
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-
         transform.Translate(Vector3.forward * basicSpeed * Time.deltaTime);
 
+        if (Input.GetKey(KeyCode.D) && moving==false && right==false)
+        {
+            rightMovement = true;
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right + distancia);
- 
+            for (count = 1; count >=0; count =(count - 0.1f * Time.deltaTime))
+            {
+                //Debug.Log("TEST COUNT = " + count);
+            }
+            Debug.Log("TEST COUNT = " + count);
+
+            if (count <= 0)
+            {
+                count = 1;
+                if (center)
+                {
+                    //transform.Translate(Vector3.right + distancia);
+                    //transform.position = Vector3.Lerp(transform.position,transform.position + distancia, 1 * Time.deltaTime);
+
+                    center = false;
+                    right = true;
+
+                }
+                else if (left)
+                {
+                    //transform.Translate(Vector3.right + distancia);
+
+                    center = true;
+                    left = false;
+
+                }
+            }
+           
+            
         }
-        if (Input.GetKey(KeyCode.S))
+
+        if (Input.GetKey(KeyCode.A) && moving == false && left==false)
         {
-            transform.Translate(Vector3.back * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left - distancia);
+            leftMovement = true;
+
+            for (count = 1; count >= 0; count = (count - 0.1f * Time.deltaTime))
+            {
+                //Debug.Log("TEST COUNT = " + count);
+            }
+
+            if (count==0)
+            {
+
+            }
+            if (center)
+            {
+                //transform.Translate(Vector3.left - distancia);
+
+                center = false;
+                left = true;
+            }
+            else if (right)
+            {
+                //transform.Translate(Vector3.left - distancia);
+
+                center = true;
+                right = false;
+            }
+
         }
 
         if (Input.GetAxisRaw("Horizontal") != 0.0f | Input.GetAxisRaw("Vertical") != 0.0f)
@@ -60,6 +116,8 @@ public class playerMovement_Traslation : MonoBehaviour
         }
         else
         {
+            rightMovement = false;
+            leftMovement = false;
             moving = false;
         }
 
